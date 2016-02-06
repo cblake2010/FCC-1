3f613fda0ef6d7d1aa8533466266e70d77c0e05a
Tim Fuller <tfuller82@gmail.com>
2016-02-06 17:26:39 -0500

    Quotes added

--- randomQuote/js/quote.js
+++ randomQuote/js/quote.js
@@ -4,20 +4,44 @@ function getRandomInt(min, max) {
 
 var quotes = [
     {
-        "text": "Akira",
-        "author": "Laine"
+        "text": "When I die, I want to go peacefully like my grandfather did–in his sleep. Not yelling and screaming like the passengers in his car.",
+        "author": "Bob Monkhouse"
     },
     {
-        "text": "Harry",
-        "author": "Potter"
+        "text": "I have six locks on my door all in a row. When I got out, I lock every other one. I figure no matter how long somebody stands there picking the locks, they are always locking three.",
+        "author": "Elayne Boosler"
     },
     {
-        "text": "Sherlock",
-        "author": "Holmes"
+        "text": "Always borrow money from a pessimist. He won't expect it back.",
+        "author": "Oscar Wilde"
     },
     {
-        "text": "Kristian",
-        "author": "Vos"
+        "text": "The scientific theory I like best is that the rings of Saturn are composed entirely of lost airline luggage.",
+        "author": "Mark Russel"
+    },
+    {
+        "text": "Friendship is like peeing on yourself: everyone can see it, but only you get the warm feeling that it brings.",
+        "author": "Robert Block"
+    },
+    {
+        "text": "First the doctor told me the good news: I was going to have a disease named after me.",
+        "author": "Steve Martin"
+    },
+    {
+        "text": "A successful man is one who makes more money than his wife can spend. A successful woman is one who can find such a man.",
+        "author": "Lana Turner"
+    },
+    {
+        "text": "How do you get a sweet little 80-year-old lady to say the F word? Get another sweet little 80-year-old lady to yell \"BINGO\"!",
+        "author": "anonymous"
+    },
+    {
+        "text": "My therapist told me the way to achieve true inner peace is to finish what I start. So far I've finished two bags of M&Ms and a chocolate cake. I feel better already.",
+        "author": "Dave Barry"
+    },
+    {
+        "text": "Dogs have masters. Cats have staff.",
+        "author": "Anonymous"
     }
 ];
 
