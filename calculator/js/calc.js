2304e9d82ad14a5f0b7ebefddf9f07e8149ec927
Tim Fuller <tfuller82@gmail.com>
2016-02-09 09:57:36 -0500

    Corrected logic error at appendValue

--- calculator/js/calc.js
+++ calculator/js/calc.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
         bool = true;
         break;
       default:
-      bool = false;
+        bool = false;
     }
     return bool;
 
@@ -71,31 +71,29 @@ $(document).ready(function() {
 
   calculatorObj.prototype.appendValue = function(str) {
     //If the first element of the array == the solution
-    //We are at answer status
-    if (this.calcArr[0] === this.solution) {
-      //If str ! Operator All Clear Before Push
-      if (!str.isOperator()) {
-        this.allClear();
-      }
-      this.calcArr.push(str);
-      this.calcDisplay = this.calcArr.join(" ");
-      this.updateDisplay();
+    //We are at answer status and str ! operator
+    if ((this.calcArr[0] === this.solution) && (!str.isOperator())) {
+      this.allClear();
     }
-    return this;
+
+    this.calcArr.push(str);
+    this.calcDisplay = this.calcArr.join(" ");
+    this.updateDisplay();
   }
+  return this;
+}
 
-  calculatorObj.prototype.calc = function() {
-    this.solution = eval(calculator.calcArr.join(""));
-    this.calcDisplay = this.solution;
-    calculator.calcArr = [];
-    calculator.calcArr.push(this.solution);
-    return this;
-  }
+calculatorObj.prototype.calc = function() {
+  this.solution = eval(calculator.calcArr.join(""));
+  this.calcDisplay = this.solution;
+  calculator.calcArr = [];
+  calculator.calcArr.push(this.solution);
+  return this;
+}
 
-  calculator = new calculatorObj();
-  calculator.allClear();
+calculator = new calculatorObj(); calculator.allClear();
 
-  $(".calc-button").click(function() {
-    calculator = buttonPress(calculator, $(this).html());
-  });
+$(".calc-button").click(function() {
+  calculator = buttonPress(calculator, $(this).html());
+});
 });
